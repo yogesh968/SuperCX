0e21f22d37d64eece8e4c0dc2db6631756f60c5e
Yogesh <yogesh968@github.com>
2026-02-23 23:06:46 +0530

    chore: Add barrel index.js for clean component imports

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+/**
+ * index.js – Barrel export for all components
+ * Import from 'src/components' for clean imports.
+ */
+
+export { default as ProgressBar } from './ProgressBar';
+export { default as DonateButton } from './DonateButton';
+export { default as Badge } from './Badge';
+export { default as UrgentCampaignCard } from './UrgentCampaignCard';
+export { default as ImpactStoryCard } from './ImpactStoryCard';
+export { default as SectionHeader } from './SectionHeader';
